0554331be16d10f9d5e8c6c242f8912725f2aa40
Dimitar-Totov <dimitar.totov@abv.bg>
2026-01-31 13:36:49 +0200

    Add select option for the type of the exercise

--- src/screens/CatalogScreen.jsx
+++ src/screens/CatalogScreen.jsx
@@ -1,36 +1,57 @@
-import { useState } from "react";
-import { StyleSheet, Text, View, TextInput, TouchableWithoutFeedback, Keyboard, TouchableOpacity } from "react-native";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
-import { X } from 'lucide-react-native';
+import { ArrowRight } from 'lucide-react-native';
 
 export default function CatalogScreen() {
-    const [text, setText] = useState('');
 
     return (
         <SafeAreaView style={styles.container}>
-            <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-                <View style={styles.content}>
-                    <View style={{ width: '100%', alignItems: 'center' }}>
-                        <TextInput style={styles.search} value={text} onChangeText={setText} placeholder='Search about some exercise' />
-                        {text ?
-                            <TouchableOpacity style={styles.clearInput} hitSlop={10} onPress={() => setText('')}>
-                                <X />
-                            </TouchableOpacity>
-                            : ''
-                        }
-                    </View>
+            <View style={styles.content}>
+                <View style={styles.selectOptions}>
+                    <Text style={styles.selectOption}>Upper Body</Text>
+                    <Text style={styles.selectOption}>Lower Body</Text>
+                    <Text style={styles.selectOption}>Full Body</Text>
+                </View>
+                <View style={styles.exercises}>
                     <View style={styles.exercise}>
                         <Text>Bench Press</Text>
+                        <ArrowRight />
                     </View>
                     <View style={styles.exercise}>
-                        <Text>Pull Up</Text>
+                        <Text>Incline Bench Press</Text>
+                        <ArrowRight />
                     </View>
                     <View style={styles.exercise}>
-                        <Text>Squat</Text>
+                        <Text>Decline Bench Press</Text>
+                        <ArrowRight />
+                    </View>
+                    <View style={styles.exercise}>
+                        <Text>Dumbbell Bench Press</Text>
+                        <ArrowRight />
+                    </View>
+                    <View style={styles.exercise}>
+                        <Text>Chest Fly</Text>
+                        <ArrowRight />
+                    </View>
+                    <View style={styles.exercise}>
+                        <Text>Cable Fly</Text>
+                        <ArrowRight />
+                    </View>
+                    <View style={styles.exercise}>
+                        <Text>Push-Ups</Text>
+                        <ArrowRight />
+                    </View>
+                    <View style={styles.exercise}>
+                        <Text>Dips</Text>
+                        <ArrowRight />
+                    </View>
+                    <View style={styles.exercise}>
+                        <Text>Machine Chest Press</Text>
+                        <ArrowRight />
                     </View>
                 </View>
-            </TouchableWithoutFeedback>
+            </View>
         </SafeAreaView>
     )
 }
@@ -41,24 +62,38 @@ const styles = StyleSheet.create({
     },
     content: {
         width: '100%',
+        marginTop: 10,
         alignItems: 'center',
+        gap: 15
     },
-    search: {
-        backgroundColor: 'white',
-        width: '92%',
-        height: 45,
-        marginBottom: 25,
-        paddingLeft: 20,
+    selectOptions:{
+        width: '100%',
+        flexDirection: 'row',
+        justifyContent: 'space-evenly'
+    },
+    selectOption:{
+        backgroundColor: '#65da5a',
+        paddingInline: 15,
+        paddingBlock: 10,
+        borderRadius: 15,
+        color: '#fff',
+        fontWeight: 'bold',
+        fontSize: 15
+    },
+    exercises: {
+        width: '90%',
+        gap: 10,
+    },
+    exercise: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        padding: 15,
+        backgroundColor: '#fff',
         borderRadius: 20,
         shadowColor: '#000',
         shadowOffset: { width: 0, height: 3 },
         shadowOpacity: 0.15,
         shadowRadius: 6,
         elevation: 4,
-    },
-    clearInput: {
-        position: 'absolute',
-        top: 11,
-        right: 30,
     }
 })
